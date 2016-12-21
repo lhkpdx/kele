2574ba9c8af7294ad82ec92e048adb04b1efb7cc
Laura Kibben <smartgirlpdx@gmail.com>
2016-12-20 16:07:03 -0800

    Lesson 3 complete

---
+++ lib/kele.rb
@@ -0,0 +1,27 @@
+require 'httparty'
+require 'json'
+
+
+class Kele
+  include HTTParty
+
+
+  def initialize(email, password)
+    response = self.class.post(api_url('sessions'), body: {"email": email, "password": password})
+    raise "Invalid email or password" if response.code == 404
+    @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
+  end
+
+
+  private
+
+  def api_url(endpoint)
+    "https://www.bloc.io/api/v1/#{endpoint}"
+  end
+
+end
